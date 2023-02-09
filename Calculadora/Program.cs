f55cedda428a4c59e78391d910209513b0d344db
WADAAA2020 <87211924+WADAAA2020@users.noreply.github.com>
2023-02-09 11:55:43 -0300

    Add files via upload

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -13,44 +13,58 @@ namespace Calculadora
             double valor1 = 0;
             double valor2 = 0;
             string opcao = "";
-
-            Console.WriteLine("Digite o primeiro valor: ");
-            valor1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo valor: ");
-            valor2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("    Qual operação você quer fazer?");
-            Console.WriteLine("    1- Soma");
-            Console.WriteLine("    2- Subtração");
-            Console.WriteLine("    3- Divisão");
-            Console.WriteLine("    4- Multiplicação");
-            Console.WriteLine("    5- Nada, só olhando mesmo." + "\n");
-            opcao = Console.ReadLine();
-            
-            if (opcao == "1")
+            while (opcao != "parar")
             {
-                Console.WriteLine("Soma é " + (valor1 + valor2));
-            }
 
-            else if (opcao == "2")
-            {
-                Console.WriteLine("Subtração é " + (valor1 - valor2));
-            }
 
-            else if (opcao == "3")
-            {
-                Console.WriteLine("Divisão é " + (valor1 / valor2));
-            }
+                Console.WriteLine("Digite o primeiro valor: ");
+                valor1 = Convert.ToDouble(Console.ReadLine());
 
-            else if (opcao == "4")
-            {
-                Console.WriteLine("Multiplicação é " + (valor1 * valor2));
-            }
-            else if (opcao == "5")
-            {
-                Console.WriteLine("Ok!");
-            }
+                Console.WriteLine("Digite o segundo valor: ");
+                valor2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("    Qual operação você quer fazer?");
+                Console.WriteLine("    1- Soma");
+                Console.WriteLine("    2- Subtração");
+                Console.WriteLine("    3- Divisão");
+                Console.WriteLine("    4- Multiplicação");
+                Console.WriteLine("    5- Nada, só olhando mesmo." + "\n");
+
+                switch (Console.ReadLine())
+                {
+
+                    case "1":
+                        Console.WriteLine("Soma é " + (valor1 + valor2)); break;
+
+                    case "2":
+                        Console.WriteLine("Subtração é " + (valor1 - valor2)); break;
+
+                    case "3":
+
+                        if (valor1 >= valor2)
+                        {
+                            Console.WriteLine("Divisão é " + (valor1 / valor2));
+                        }
+                        else
+                        {
+                            Console.WriteLine("invalido");
+                        }
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Multiplicação é " + (valor1 * valor2)); break;
+
+                    case "5":
+                        Console.WriteLine("Ok!"); break;
+
+                }
+
+                Console.WriteLine("Deseja continuar?");
+                opcao = Console.ReadLine();
+
+                Console.ReadKey();
+
+            }        
         }
     }
 }
